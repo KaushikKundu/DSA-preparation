2970c8af47387b86e42491faca3bff813de95332
Kaushik Kundu <kaushikkundu774@gmail.com>
2023-11-07 23:02:19 +0530

    day 2

---
+++ src/Sorting/insertionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class insertionSort {
+    public static void insertionSort(int n , int[] arr) {
+        // Write your code here.
+        for(int i=1; i<n; i++){
+            int temp = arr[i];
+
+            int j = i-1;
+            while(j>=0){
+                if( arr[j] > temp ){
+                    arr[j+1] = arr[j];
+                }else{
+                    break;
+                }
+                j--;
+            }
+
+            arr[j+1] = temp;
+        }
+    }
+}
---
+++ src/Sorting/selectionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class selectionSort {
+    public static void selectionSort(int[] arr) {
+        //Your code goes here
+
+        int temp = 0;
+        for(int i=0; i<arr.length-1; i++){
+            int min = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+    }
+}
+}
