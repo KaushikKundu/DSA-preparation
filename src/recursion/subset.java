4d3cd8cd5a5a0f1e93aefd36eaf71ee489862289
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-02-06 00:57:20 +0530

    subset patterns

--- src/recursion/subset.java
+++ src/recursion/subset.java
@@ -1,13 +1,19 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class subset {
     public static void main(String[] args) {
         //noOfSubset("","abc");
-        ArrayList<String> arr = new ArrayList<>();
-
-        System.out.println( arrOfSubset("","abc",arr));
+        int[] arr = {1,2,3};
+        List<List<Integer>> list = iterativeSubset(arr);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        //ArrayList<String> arr = new ArrayList<>();
+        //System.out.println(iterativeSubset());
+        //System.out.println( arrOfSubset("","abc",arr));
     }
     static void noOfSubset(String p, String up){
         if(up.isEmpty()){
@@ -27,5 +33,18 @@ public class subset {
         arrOfSubset(p+ch,up.substring(1),arr);
         arrOfSubset(p,up.substring(1),arr);
         return arr;
+    }
+    static List<List<Integer>> iterativeSubset(int[] arr){
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>());
+        for(int num : arr){
+            int n = list.size();
+            for (int i = 0; i < n ; i++) {
+                List<Integer> internal = new ArrayList<>(list.get(i));
+                internal.add(num);
+                list.add(internal);
+            }
+        }
+        return list;
     }
 }
