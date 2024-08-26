ea70fed57d10455ff6fe68f7b1d77695779e6353
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-08-26 12:32:47 +0530

    permutations

--- recursion/permutation.java
+++ recursion/permutation.java
@@ -1,29 +1,19 @@
-package recursion;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class permutation {
     public static void main(String[] args) {
         String str = "abc";
-        recurPermute("",str);
-        //ArrayList<String> list = recurPermute("",str);
-        //System.out.println(list);
-    }
-    static void recurPermute(String p, String up ){
+        solve("",str);
+    };
+    private static void solve(String p, String up){
         if(up.isEmpty()){
-            //ArrayList<String> list = new ArrayList<>();
             System.out.println(p);
-            return ;
+            return;
         }
         char ch = up.charAt(0);
-        //ArrayList<String> ans = new ArrayList<>();
-        for (int i = 0; i < p.length(); i++) {
+        for(int i=0; i<= p.length(); i++){
             String l = p.substring(0,i);
             String r = p.substring(i);
-
-        recurPermute(l + ch + r,up.substring(1));
+            solve(l+ch+r,up.substring(1));
         }
-
     }
 }
